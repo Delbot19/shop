aa3edc15426c7aef0704bef2e73306c899d905e1
Delbot19 <delbotoussou00@gmail.com>
2023-04-05 15:13:19 +0000

    Updates

---
+++ precache-manifest.8f1d12b20a05e05d074b414d93eb21c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c4d285714d5e6361e3a",
+    "url": "/shop/static/css/main.cda739e2.chunk.css"
+  },
+  {
+    "revision": "8c4d285714d5e6361e3a",
+    "url": "/shop/static/js/main.8c4d2857.chunk.js"
+  },
+  {
+    "revision": "adb2852fae2b928deb4c",
+    "url": "/shop/static/js/1.adb2852f.chunk.js"
+  },
+  {
+    "revision": "a91abfb36e87675f48a7",
+    "url": "/shop/static/css/2.d3eda7b5.chunk.css"
+  },
+  {
+    "revision": "a91abfb36e87675f48a7",
+    "url": "/shop/static/js/2.a91abfb3.chunk.js"
+  },
+  {
+    "revision": "a8c16cf6bd26d073e778",
+    "url": "/shop/static/js/runtime~main.a8c16cf6.js"
+  },
+  {
+    "revision": "d316eda36a6df2c4281ff6e9b4cb949a",
+    "url": "/shop/static/media/Special Item.d316eda3.jpg"
+  },
+  {
+    "revision": "6f6c87c7f7a5e8b2e0fa3c8607b9c087",
+    "url": "/shop/static/media/Fancy.6f6c87c7.jpg"
+  },
+  {
+    "revision": "2f2696d08ba75839a98f79259b8d6849",
+    "url": "/shop/static/media/Fancy Product1.2f2696d0.jpeg"
+  },
+  {
+    "revision": "fd06e0a13e6245295e7667e303a5e390",
+    "url": "/shop/static/media/Popular Item.fd06e0a1.jpg"
+  },
+  {
+    "revision": "9da73c0c8cf6d0bcaf7a4cd122aef576",
+    "url": "/shop/static/media/Popular Item1.9da73c0c.jpg"
+  },
+  {
+    "revision": "171b1b5118769e9fb8167085f3f2c489",
+    "url": "/shop/static/media/Sale Item1.171b1b51.jpeg"
+  },
+  {
+    "revision": "815202e76b20297700c073daf5c8d917",
+    "url": "/shop/static/media/Special Item1.815202e7.jpg"
+  },
+  {
+    "revision": "179ab96c288214c10a728977a0464f8e",
+    "url": "/shop/index.html"
+  }
+];
