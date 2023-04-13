8dba2684f1934a67186e3f4931c21d4714754200
Delbot19 <delbotoussou00@gmail.com>
2023-04-13 12:53:17 +0000

    debut cart

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,18 @@ import Banner from './Banner';
 import Caroussel from './Caroussel';
 import ShopingList from './ShopingList';
 import Footer from './Footer';
+import Cart from './Cart';
 
 function App() {
   const [compteur,setCompteur]=useState(0)
-
+  const [cart,updateCart]=useState([])
   return (
     <div className="App">
       <Banner compteur={compteur} setCompteur={setCompteur} />
       <Caroussel/>
-      <ShopingList compteur={compteur} setCompteur={setCompteur} />
-      <Footer/>
+      <Cart cart={cart} updateCart={updateCart}/>
+      <ShopingList compteur={compteur} setCompteur={setCompteur} cart={cart} updateCart={updateCart} />
+      <Footer/> 
     </div>
   );
 }
