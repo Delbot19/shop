42d09192190849e99f723ea3b352724c7bd9f2b8
Delbot19 <delbotoussou00@gmail.com>
2023-04-18 14:40:05 +0000

    Updates

---
+++ precache-manifest.4ec06aaf28bc019dff4ba8f2a5257167.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c732e58d1b653bfb53e",
+    "url": "/Shop/static/css/main.52e454d0.chunk.css"
+  },
+  {
+    "revision": "7c732e58d1b653bfb53e",
+    "url": "/Shop/static/js/main.7c732e58.chunk.js"
+  },
+  {
+    "revision": "de777e5fde807a0452ce",
+    "url": "/Shop/static/js/1.de777e5f.chunk.js"
+  },
+  {
+    "revision": "e0c64b4ca7970009a6b9",
+    "url": "/Shop/static/css/2.60f1ed76.chunk.css"
+  },
+  {
+    "revision": "e0c64b4ca7970009a6b9",
+    "url": "/Shop/static/js/2.e0c64b4c.chunk.js"
+  },
+  {
+    "revision": "1265ae6c8457afcae7d6",
+    "url": "/Shop/static/js/runtime~main.1265ae6c.js"
+  },
+  {
+    "revision": "5be7f2e86c75a305bce60f39e28fcc45",
+    "url": "/Shop/static/media/Fancy.5be7f2e8.jpg"
+  },
+  {
+    "revision": "481801ccca58e979b57dce407b219f8a",
+    "url": "/Shop/static/media/Fancy Product1.481801cc.jpg"
+  },
+  {
+    "revision": "14ee3a01399f202cd75467c083b093bf",
+    "url": "/Shop/static/media/Popular Item.14ee3a01.jpg"
+  },
+  {
+    "revision": "f1a27e2b210bd7b67b30f56c6a555dcb",
+    "url": "/Shop/static/media/Special Item.f1a27e2b.jpg"
+  },
+  {
+    "revision": "f2e01bac9e9517c93ba2915e678ae16d",
+    "url": "/Shop/static/media/mannequin.f2e01bac.png"
+  },
+  {
+    "revision": "4cca055e03c6b6edc15b97e3d5ebab72",
+    "url": "/Shop/static/media/mannequin3.4cca055e.png"
+  },
+  {
+    "revision": "6e3ac16363344d7f4ad6f16f8631acef",
+    "url": "/Shop/static/media/Popular Item1.6e3ac163.jpg"
+  },
+  {
+    "revision": "d7b152409cc764fd334146f4a1a9a56d",
+    "url": "/Shop/static/media/Sale Item1.d7b15240.jpg"
+  },
+  {
+    "revision": "f075832417a11c19b4a870244f41d51b",
+    "url": "/Shop/static/media/Special Item1.f0758324.jpg"
+  },
+  {
+    "revision": "1b2737a8cf1626141981dbe6aab0d49c",
+    "url": "/Shop/static/media/Jackets.1b2737a8.jpg"
+  },
+  {
+    "revision": "32c4c4527d785140438b4cb7758e7871",
+    "url": "/Shop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shop/precache-manifest.b3de14d384c32715b91f6a52e66a0229.js"
+  "/Shop/precache-manifest.4ec06aaf28bc019dff4ba8f2a5257167.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/shop/index.html", {
+workbox.routing.registerNavigationRoute("/Shop/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
